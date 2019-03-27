a9aa253fea86657b691aa67bc504c16b53e202ff
Angela Mumba <malaika.mumba@gmail.com>
2019-03-27 01:37:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,21 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
+<<<<<<< HEAD
   if (num % 3 == 0) && (num % 5 == 0)
     return "FizzBuzz"
     
     elsif num % 5 == 0
     return "Buzz"
     
+=======
+  
+  
+  if (num % 3 == 0) && (num % 5 == 0)
+    return "fizzbuzz"
+    elsif num % 5 == 0
+    return "Buzz"
+>>>>>>> 57f373163a5318432212f9fb18e63030454270e7
     elsif num % 3 == 0 
     return "Fizz"
     
